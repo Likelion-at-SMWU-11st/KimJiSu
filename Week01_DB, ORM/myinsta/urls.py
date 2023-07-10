def15ab49f8110dce1c1d339cb10ddb51b8e546d
Kim Jisu <xoxo1795@sookmyung.ac.kr>
2023-07-11 00:25:08 +0900

    function based view

--- Week01_DB, ORM/myinsta/urls.py
+++ Week01_DB, ORM/myinsta/urls.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from django.urls import path
 
 from posts.views import url_view
+from posts.views import url_parameter_view
+from posts.views import function_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('url/', url_view),
+    path('url/<int:username>/', url_parameter_view),
+    path('fbv/', function_view),
 ]
--- Week01_DB, ORM/posts/views.py
+++ Week01_DB, ORM/posts/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 def url_view(request):
-    return HttpResponse('url.view')
+    data = {'code': '001', 'msg': 'OK'}
+    return HttpResponse('<h1>url_views</h1>')
+
+def url_parameter_view(request, username):
+    print('url_parameter_view()')
+    print(f'username: {username}')
+    print(f'request.GET: {request.GET}')
+    return HttpResponse(username)
+
+def function_view(request):
+    print(f'request.method: {request.method}')
+    print(f'request.GET: {request.GET}')
+    print(f'request.POST: {request.POST}')
+    return render(request, 'view.html')
