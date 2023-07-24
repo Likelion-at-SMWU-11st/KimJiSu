62c84a20c17e1777a6c21b808c09132bb540aaaa
Kim Jisu <xoxo1795@sookmyung.ac.kr>
2023-07-25 02:08:10 +0900

    [Feat] app - users

--- week04_CRUD/myinsta/settings.py
+++ week04_CRUD/myinsta/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,6 @@ MEDIA_ROOT = BASE_DIR / 'myinsta' / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'users.User'
